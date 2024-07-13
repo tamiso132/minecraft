57134ee7ff6855b2a1122dca833d59e5fe8689db
tamiso132 <tomasxrandi@hotmail.se>
2024-07-13 17:44:47 +0200

    u

--- build.rs
+++ build.rs
@@ -5,7 +5,16 @@ use std::process::Command;
 fn main() {
     // Note that there are a number of downsides to this approach, the comments
     // below detail how to improve the portability of these commands.
-    Command::new("chmod").args(["+x", "build_shader.sh"]);
+    build_shaders();
+}
 
+#[cfg(target_os = "linux")]
+fn build_shaders() {
+    Command::new("chmod").args(["+x", "build_shader.sh"]);
     Command::new("bash").arg("build_shader.sh").status().unwrap();
+}
+
+#[cfg(target_os = "windows")]
+fn build_shaders() {
+    Command::new("./build_shader.bat").status().unwrap();
 }
